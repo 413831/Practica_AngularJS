ef114772f316c65c2786236540f51f3c8330e01d
413831 <alepleins3pp@gmail.com>
2020-05-13 13:45:27 -0300

    Update

--- Curso_Angular/Angular/01-hola-mundo/src/app/app.component.ts
+++ Curso_Angular/Angular/01-hola-mundo/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Proyecto 1 - Hola mundo';
+  nombre = 'Pepito';
+  apellido = 'Marranos';
 }
