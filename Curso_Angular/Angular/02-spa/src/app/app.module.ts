60dc720dbd0dd8efc5c6c92cf5d552eeabd8bc61
413831 <alepleins3pp@gmail.com>
2020-05-14 19:20:36 -0300

    Actualizacion curso

--- Curso_Angular/Angular/02-spa/src/app/app.module.ts
+++ Curso_Angular/Angular/02-spa/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+// Rutas
+import { ROUTING } from "./app.routes";
+
+
+// Servicios
+
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ROUTING,
   ],
   providers: [],
   bootstrap: [AppComponent]
