1487e7bf559d7897d6fb5eff9649856fa297ef9f
413831 <alepleins3pp@gmail.com>
2021-01-25 09:59:22 -0300

    Update Java

--- Curso_JAVA/Fundamentos/src/com/curso/operadores/Operadores.java
+++ Curso_JAVA/Fundamentos/src/com/curso/operadores/Operadores.java
@@ -19,5 +19,17 @@ public class Operadores {
 
         resultado = a / b;
         System.out.println("resultado division = " + resultado);
+
+        resultado = a % b;
+        System.out.println("resultado módulo = " + resultado);
+
+        if (a % 2 == 0)
+        {
+            System.out.println("Es número par");
+        }
+        else
+        {
+            System.out.println("Es número impar");
+        }
     }
 }
