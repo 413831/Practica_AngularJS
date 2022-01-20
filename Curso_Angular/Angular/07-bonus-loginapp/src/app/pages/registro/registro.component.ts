4e1d37e8daa7452f58059b4bdea3a94f867f7644
413831 <alepleins3pp@gmail.com>
2022-01-19 21:04:25 -0300

    Introduccion a Python 1

---
+++ Curso_Angular/Angular/07-bonus-loginapp/src/app/pages/registro/registro.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit 
+{
+
+  usuario:UsuarioModel;
+
+  constructor() { }
+
+  ngOnInit() 
+  {
+    this.usuario = new UsuarioModel();
+
+    this.usuario.email = 'mail@mail.com';
+  }
+
+  onSubmit(form: NgForm)
+  {
+    if(form.invalid) return;
+
+    console.log("Formulario enviado");
+    console.log(this.usuario);
+    console.log(form);
+  }
+
+
+
+}
