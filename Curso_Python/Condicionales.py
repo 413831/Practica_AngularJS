2fa0f94f24f8f3e4b7c3dfefb6581ad519d01840
413831 <alepleins3pp@gmail.com>
2022-01-21 19:42:12 -0300

    Ciclo For Python

--- Curso_Python/Condicionales.py
+++ Curso_Python/Condicionales.py
@@ -4,3 +4,12 @@ if numeros > 0:
     print("positivo")
 else:
     print("negativo")
+
+if (numeros & 2) == 0:
+    print("Numero par")
+elif numeros == 0:
+    print("Cero")
+else:
+    print("Numero impar")
+
+
