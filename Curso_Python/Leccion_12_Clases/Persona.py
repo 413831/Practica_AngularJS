93adf8760ca3af1cc2634b72777367db57423256
413831 <alepleins3pp@gmail.com>
2022-03-15 08:22:48 -0300

    Ejercicios clases 1

--- Curso_Python/Leccion_12_Clases/Persona.py
+++ Curso_Python/Leccion_12_Clases/Persona.py
@@ -1,14 +1,27 @@
 class Persona:
     # constructor
     # método double underscore o dunder
-    def __init__(self):
-        self.nombre = 'Juan'
-        self.apellido = 'Perez'
-        self.edad = 28
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
 
-persona1 = Persona()
-print(persona1.nombre)
-print(persona1.apellido)
-print(persona1.edad)
+    def mostrar_detalle(self):
+        print(f'Persona: {self.nombre} {self.apellido} {self.edad} años')
+
+
+# nota: se pueden crear atributos de clase de forma dinamica
+Persona.telefono = 143243242
+
+persona1 = Persona('Pepito','Gonzalez',28)
+# persona1.mostrar_detalle()
+Persona.mostrar_detalle(persona1)
+print(persona1.telefono)
+
+persona2 = Persona('Carmen','Sandiego', 45)
+# persona2.mostrar_detalle()
+Persona.mostrar_detalle(persona2)
+print(persona2.telefono)
+
 
 
