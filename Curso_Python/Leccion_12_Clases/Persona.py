56e2bdaa2afbafff3403c85be126e288b3420680
413831 <alepleins3pp@gmail.com>
2022-03-18 14:47:39 -0300

    Herencia

--- Curso_Python/Leccion_12_Clases/Persona.py
+++ Curso_Python/Leccion_12_Clases/Persona.py
@@ -33,20 +33,27 @@ class Persona:
     def edad(self,edad):
         self.edad = edad
 
+    def __del__(self):
+        print(f'Persona {self.nombre} {self.apellido}')
 
-# nota: se pueden crear atributos de clase de forma dinamica
-Persona.telefono = 143243242
 
-persona1 = Persona('Pepito','Gonzalez',28)
-# persona1.mostrar_detalle()
-Persona.mostrar_detalle(persona1)
-print(persona1.telefono)
+# la variable global __name__ indica el módulo que se ejecuta
+if __name__ == '__main__':
+    # nota: se pueden crear atributos de clase de forma dinamica
+    Persona.telefono = 143243242
 
-persona2 = Persona('Carmen','Sandiego', 45)
-# persona2.mostrar_detalle()
-Persona.mostrar_detalle(persona2)
-print(persona2.telefono)
+    persona1 = Persona('Pepito', 'Gonzalez', 28)
+    # persona1.mostrar_detalle()
+    Persona.mostrar_detalle(persona1)
+    print(persona1.telefono)
 
-# nota: con el decorador property se accede a los atributos indirectamente
-print(persona2.nombre)
+    persona2 = Persona('Carmen', 'Sandiego', 45)
+    # persona2.mostrar_detalle()
+    Persona.mostrar_detalle(persona2)
+    print(persona2.telefono)
 
+    # nota: con el decorador property se accede a los atributos indirectamente
+    print(persona2.nombre)
+
+
+print(__name__)
