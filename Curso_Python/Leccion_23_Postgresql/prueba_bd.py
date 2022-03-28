be5a95ce73ae8aedc116c9cc28ae9de32c4fbab2
413831 <alepleins3pp@gmail.com>
2022-03-28 12:45:22 -0300

    Transacciones a DB

--- Curso_Python/Leccion_23_Postgresql/prueba_bd.py
+++ Curso_Python/Leccion_23_Postgresql/prueba_bd.py
@@ -1,26 +1,61 @@
-import psycopg
+import psycopg as bd
 
-conexion = psycopg.connect(
-    user='postgres',
-    password='admin',
-    host='127.0.0.1',
-    port='5432',
-    dbname='test_db'
-)
+conexion = bd.connect(user='postgres',password='admin',host='127.0.0.1',port='5432',dbname='test_db')
 
-print(conexion)
+#print(conexion)
 # Se recupera cursor para comandos SQL
-cursor = conexion.cursor()
-# Objeto para sentencia
-sentencia = 'SELECT * FROM personas'
-# Se ejecuta QUERY a table
-cursor.execute(sentencia)
-# Se recuperan los registros de la QUERY
-registro = cursor.fetchall()
+# cursor = conexion.cursor()
 
-print(registro)
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'DELETE FROM personas WHERE id_persona = ANY(%s)'
+            entrada = input('Proporciona los id_persona para eliminar (separado por comas): ')
+            llaves_primarias = entrada.split(',')
+            cursor.executemany(sentencia,[[llaves_primarias]])
+            # conexion.commit() # Se realiza automaticamente
+            registros_eliminados = cursor.rowcount
+            print(f'Registros Eliminados: {registros_eliminados}')
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    conexion.close()
+    # Se cierra conexión a base de datos
+    # cursor.close()
 
-# Se cierra conexión a base de datos
-cursor.close()
-conexion.close()
+# -- EJEMPLOS
+# sentencia = 'SELECT * FROM personas WHERE id_persona = %s'
 
+# sentencia = 'SELECT * FROM personas WHERE id_persona = ANY(%s)' # No se utilizan tuplas de tuplas en psycopg3
+# # llaves_primarias = [[1,2,3]]
+'''
+    entrada = input('Proporciona los id\'s a buscar (separado por comas): ')
+    llaves_primarias = [entrada.split(',')]
+    print(llaves_primarias)
+    cursor.execute(sentencia, llaves_primarias) # Se ejecuta QUERY a table
+    registros = cursor.fetchall() # Se recuperan los registros de la QUERY
+    for registro in registros:
+       print(registro)
+'''
+
+# sentencia = 'INSERT INTO personas(nombre, apellido, email) VALUES(%s, %s, %s)'
+#             valores = ('Carlos','Lara','clara@mail.com')
+
+# sentencia = 'INSERT INTO personas(nombre, apellido, email) VALUES(%s, %s, %s)'
+#             valores = (
+#                 ('Maria','Lopez','mlopez@mail.com'),
+#                 ('Hermeneguildo', 'Herz', 'h.hmail@mail.com'),
+#                 ('Angel', 'Quintana', 'aquintana@mail.com')
+#             )
+#             cursor.executemany(sentencia,valores)
+
+# sentencia = 'UPDATE personas SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+#             valores = (
+#                 ('Patricio', 'Pereyra', 'p.pereyra@mail.com', 1),
+#                 ('Ivan', 'Navi', 'i_navi@mail.com', 2)
+#             )
+
+#sentencia = 'DELETE FROM personas WHERE id_persona = ANY(%s)'
+# entrada = input('Proporciona los id_persona para eliminar (separado por comas): ')
+# llaves_primarias = entrada.split(',')
+# cursor.executemany(sentencia,[[llaves_primarias]])
