9663ec5994e3c26237a7f5a4e03fbdf55abcc01c
413831 <alepleins3pp@gmail.com>
2022-04-08 23:14:50 -0300

    Profundizando operadores

--- Curso_Python/Leccion_26_ProfundizandoPython/listas.py
+++ Curso_Python/Leccion_26_ProfundizandoPython/listas.py
@@ -41,4 +41,52 @@ numeros2 = numeros1[:]
 print(f'Misma referencia? {numeros1 is numeros2}')
 print(f'Mismo contenido? {numeros1 == numeros2}')
 
+# Multiplicación listas
+lista_multiplicacion = 5*[[2, 5]]
+print(lista_multiplicacion)
+print(f'Misma referencia: {lista_multiplicacion[0] is lista_multiplicacion[1]}')
+print(f'Misma contenido: {lista_multiplicacion[0] is lista_multiplicacion[1]}')
+lista_multiplicacion[2].append(10)
+print(lista_multiplicacion)
+
+# Matrices en Python
+matriz = [[10, 20], [30,40,50],[60,70,80,90]]
+print(f'Matriz original: {matriz}')
+print(f'Fila 0, Columna 0: {matriz[0][0]}')
+print(f'Fila 2, Columna 2: {matriz[2][2]}')
+matriz[2][0] = 65
+print(f'Matriz modificada: {matriz}')
+
+lista_listas = [[10,14,87,90,71],[4,5,6,7],[9,0,11,15,45,61,70]]
+# Ordenar por el length de la lista
+lista_listas.sort(key=len)
+print(f'Ordenar lista: {lista_listas}')
+
+# sorted built-in
+# help(sorted)
+nombres1 = ['Juan Carlos', 'Karla','Pedro','Esperanza']
+nombres1 = sorted(nombres1)
+print(nombres1)
+# Ordenar de manera descendente
+nombres1 = sorted(nombres1, reverse=True)
+print(nombres1)
+# Ordenar por la cantidad de caracteres (largo)
+nombres1 = sorted(nombres1,key=len)
+print(nombres1)
+# built-in reversed
+nombres1 = reversed(nombres1)
+print(list(nombres1))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
