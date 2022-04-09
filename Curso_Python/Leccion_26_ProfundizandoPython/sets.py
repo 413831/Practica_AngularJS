d539c4ad546fc6c47120039bbded000f0c1b467e
413831 <alepleins3pp@gmail.com>
2022-04-09 20:09:32 -0300

    Scope y funciones

--- Curso_Python/Leccion_26_ProfundizandoPython/sets.py
+++ Curso_Python/Leccion_26_ProfundizandoPython/sets.py
@@ -39,10 +39,34 @@ pelo_rubio = {'Kevin','Laura','Marcos'}
 ojos_cafe = {'Karla','Laura','Juan'}
 menores_30 = {'Juan','Karla','Maria'}
 
-# Todos los elementos con ojos color cafe y pelo rubio (Union)
+# Union - Todos los elementos con ojos color cafe y pelo rubio
+# nota: es una operacion conmutativa
 print(ojos_cafe.union(pelo_rubio))
-# Invertir el orden con el mismo resultado (conmutativa)
-print(pelo_rubio.union(ojos_cafe))
+# Invertir el orden con el mismo resultado
+print(f'Union: {pelo_rubio.union(ojos_cafe)}')
+
+# Intersection (AND) - Sólo las personas con ojos café y pelo rubio
+# nota: operacion conmutativa
+print(f'Intersection: {ojos_cafe.intersection(pelo_rubio)}')
+
+# Difference - Pelo negro sin ojos cafe
+# nota: no es operacion conmutativa
+print(f'Difference: {pelo_negro.difference(ojos_cafe)}')
+
+# Symmetric Difference (XOR)
+# nota: operacion conmutativa
+print(f'Symmetric difference: {pelo_negro.symmetric_difference(ojos_cafe)}')
+
+# Subset - Preguntar si un set está contenido en otro
+# Revisamos si los elementos del primer set están contenidos en el segundo set
+print(menores_30.issubset(pelo_negro))
+
+# Superset - Preguntar si un set contiene a otro set
+# Revisar si los elementos del primer set están contenidos en el segundo set
+print(menores_30.issuperset(pelo_negro))
+
+# Disjoint - Preguntar si los elementos de pelo negro no tienen pelo rubio
+print(pelo_negro.isdisjoint(pelo_rubio))
 
 
 
