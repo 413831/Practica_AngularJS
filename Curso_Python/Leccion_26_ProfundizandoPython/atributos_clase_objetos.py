16ec82a930b256d85e4b9d908ab752877b041aaf
413831 <alepleins3pp@gmail.com>
2022-04-11 16:00:46 -0300

    Clases y atributos

---
+++ Curso_Python/Leccion_26_ProfundizandoPython/atributos_clase_objetos.py
@@ -0,0 +1,13 @@
+class Persona:
+    contador_personas = 0
+
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+# Mostrar los atributos de un objeto
+persona1 = Persona('Pepito','Gonzalez')
+print(persona1.__dict__)
+
+# Crear un atributo al vuelo
+persona1.contador_personas = 10
--- Curso_Python/Leccion_26_ProfundizandoPython/list_comprehension.py
+++ Curso_Python/Leccion_26_ProfundizandoPython/list_comprehension.py
@@ -39,3 +39,31 @@ lista_impares = []
 [lista_pares.append(numero) if numero % 2 == 0 else lista_impares.append(numero) for numero in range(10)]
 print(f'Pares: {lista_pares}')
 print(f'Impares: {lista_impares}')
+
+# Lista de listas
+lista_listas = [[1,2,3],[4,5,6],[7,8,9,10]]
+# Convertimos la lista de listas en una sola lista
+lista_simple = [valor
+                for sublista in lista_listas
+                for valor in sublista]
+print(f'Lista simple: {lista_simple}')
+# Crear una lista de números pares a partir de la lista_listas
+# Sin List comprehensions (Ciclos for anidados)
+lista_pares = []
+for sublista in lista_listas:
+    for valor in sublista:
+        if valor % 2 == 0:
+            lista_pares.append(valor)
+print(f'Lista pares: {lista_pares}')
+# Con List comprehensions
+lista_pares = []
+lista_pares = [valor for sublista in lista_listas for valor in sublista if valor % 2 == 0]
+print(f'Lista pares: {lista_pares}')
+
+
+
+
+
+
+
+
